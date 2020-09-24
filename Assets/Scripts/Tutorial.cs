c0ce81882ea8aa6ea0e335cb5df1f4c9d9f64ae4
Denis Chernitsyn <denis.chernitsyn@gmail.com>
2020-09-24 17:42:38 +0300

    add a simple tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private Canvas tutorialCanvas = null;
+    [SerializeField] private RectTransform tutorialContainer = null;
+    [SerializeField] [Range(1, 10)] private float tutorialDuration = 5;
+
+    private void Awake()
+    {
+        tutorialContainer.localScale = Vector3.zero;
+    }
+
+    void Start()
+    {
+        StartCoroutine(ShowTutorial());
+    }
+
+    private IEnumerator ShowTutorial()
+    {
+        tutorialContainer.DOScale(Vector3.one, 1.0f).SetEase(Ease.InOutCubic);
+        yield return new WaitForSeconds(tutorialDuration);
+        tutorialContainer.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InOutCubic).OnComplete(() =>
+        {
+            tutorialCanvas.gameObject.SetActive(false);
+        });
+    }
+}
